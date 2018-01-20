e082ffbaa015554fdf7a4165ce517ae49d4327e4
bambooforest@gmail.com <bambooforest@gmail.com>
2018-01-20 18:19:17 +0100

    combine the data script in regard to issue #1

---
+++ data/combine-data.sh
@@ -0,0 +1,4 @@
+cat words1.csv words2.csv words3.csv words4.csv words5.csv > words.csv
+cat morphemes1.csv morphemes2.csv morphemes3.csv > morphemes.csv
+cat ChintangMorphemes1.csv ChintangMorphemes2.csv ChintangMorphemes3.csv > ChintangMorphemes.csv
+cat RussianWords1.csv RussianWords2.csv RussianWords3.csv > RussianWords.csv
